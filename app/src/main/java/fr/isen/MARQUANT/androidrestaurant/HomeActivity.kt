97cbfbba5d30edcf6752a3fa5f3b2300801e986a
jenny <jenny.marquant@isen.yncrea.fr>
2021-01-25 13:55:30 +0100

    text question 2.c et debut bouton

--- app/src/main/java/fr/isen/MARQUANT/androidrestaurant/HomeActivity.kt
+++ app/src/main/java/fr/isen/MARQUANT/androidrestaurant/HomeActivity.kt
@@ -2,10 +2,17 @@ package fr.isen.MARQUANT.androidrestaurant
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import fr.isen.MARQUANT.androidrestaurant.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.buttonEntrees.setOnClickListener {
+           // Toast.makeText(this, getString(R.string.toast_content), Toast.LENGTH_LONG).show()
+        }
     }
 }
